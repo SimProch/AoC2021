e2a6a7e1bdf07b4ca6a9fc860223abf8b21f66e9
Simon Prochazka <simon.prochazka@logex.nl>
2021-12-09 10:45:24 +0100

    Finished d9

--- d9/day9_p2.js
+++ d9/day9_p2.js
@@ -2,13 +2,15 @@ import { d9_data, d9_test_data } from "./input.js";
 
 // https://adventofcode.com/2021/day/9
 
-dayNine(d9_test_data);
-// dayNine(d9_data);
+// dayNine(d9_test_data);
+dayNine(d9_data);
 
 function dayNine(data) {
     const emptyGrid = getEmptyGrid(data.length, data[0].length);
     findLowestPoints(data, emptyGrid);
-    const result = getBasins(data, emptyGrid);
+    const basinLengths = getBasinLengths(data, emptyGrid).sort((x,y) => y - x)
+    const [first, second, third, ...rest] = basinLengths;
+    const result = first * second * third;
     console.log(result);
 }
 
@@ -24,6 +26,7 @@ function findLowestPoints(data, emptyGrid) {
 }
 
 function getCellAndAdjacentValues(data, columnIndex, rowIndex) {
+    if (data[columnIndex]?.[rowIndex] == null) return { cellValue: null, adjacentCells: null}
     const cellValue = +data[columnIndex][rowIndex];
     const top = data[columnIndex - 1]?.[rowIndex] != null ? +data[columnIndex - 1]?.[rowIndex] : null;
     const bottom = data[columnIndex + 1]?.[rowIndex] != null ? +data[columnIndex + 1]?.[rowIndex] : null;
@@ -40,21 +43,37 @@ function isCurrentLowest(currentCell, adjacentCells) {
         (currentCell < adjacentCells.right || adjacentCells.right == null);
 }
 
-function getBasins(data, emptyGrid) {
-    let basinLength = 0;
+function getBasinLengths(data, emptyGrid) {
+    let basinLengths = []
     for (let i = 0; i < emptyGrid.length; i++) {
         for (let j = 0; j < emptyGrid[i].length; j++) {
             if (!emptyGrid[i][j]) continue;
-            const basin = getBasin(data,i,j);
-            basinLength = basin.length;
+            const coordinates = []
+            const basinLength = getBasinLength(data,i,j, coordinates);
+            basinLengths.push(basinLength);
         }
     }
-    return basinLength
+    return basinLengths
 }
 
-function getBasin(data, i, j) {
+function getBasinLength(data, i, j, coordinates) {
+    let count = 1;
     const { cellValue, adjacentCells } = getCellAndAdjacentValues(data, i, j)
-    
+    coordinates.push([j, i])
+    if (cellValue == null) return 0;
+    if (goTo(adjacentCells.top, i - 1, j, coordinates)) count += getBasinLength(data,i - 1, j, coordinates);
+    if (goTo(adjacentCells.bottom, i + 1, j, coordinates)) count += getBasinLength(data,i + 1, j, coordinates);
+    if (goTo(adjacentCells.left, i, j - 1, coordinates)) count += getBasinLength(data,i, j - 1, coordinates);
+    if (goTo(adjacentCells.right, i, j + 1, coordinates)) count += getBasinLength(data,i, j + 1, coordinates);
+    return count;
+}
+
+function goTo(adjacentCell, i, j, coordinates) {
+    if (adjacentCell == null) return false;
+    if (adjacentCell === 9) return false;
+    const alreadyAdded = coordinates.find(coord => coord[0] === j && coord[1] === i);
+    if (alreadyAdded) return false;
+    return true;
 }
 
 function getEmptyGrid(columnLength, cellLength) {

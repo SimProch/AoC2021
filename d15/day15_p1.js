915657cc29cb34b1b05149f447788eab5f3321c1
Simon Prochazka <simon.prochazka@logex.nl>
2021-12-15 07:40:04 +0100

    Init d15

---
+++ d15/day15_p1.js
@@ -0,0 +1,10 @@
+import { d15_data, d15_test_data } from "./input.js";
+
+// https://adventofcode.com/2021/day/14
+
+// dayFourteen(d15_test_data);
+dayFifteen(d15_data);
+
+function dayFifteen(data) {
+
+}

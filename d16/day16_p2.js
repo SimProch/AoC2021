f8027cfe49cb7ca46c73b4d239abcda7ffd217ad
Simon Prochazka <simon.prochazka@logex.nl>
2021-12-16 18:18:34 +0100

    d16p1

---
+++ d16/day16_p2.js
@@ -0,0 +1,135 @@
+import { d16_data } from "./input.js";
+
+// https://adventofcode.com/2021/day/16
+
+console.log(daySixteen(d16_data));
+
+function daySixteen(hex) {
+    const binary = hex2bin(hex);
+    const parsed = parsePacket(binary);
+    const versionSum = getPacketVersionSum(parsed, 0);
+    return versionSum;
+}
+
+function getPacketVersionSum(parsedPacket) {
+    if (typeof parsedPacket.value == 'number') return parsedPacket.packetVersion;
+    let result = 0;
+    parsedPacket.value.forEach(i => {
+        result += getPacketVersionSum(i);
+    })
+    result += parsedPacket.packetVersion
+    return result;
+}
+
+function parsePacket(binary, maxLengthInBits) {
+    const packetVersion = getPacketVersion(binary);
+    const packetTypeId = getPacketTypeId(binary);
+    const lengthTypeId = getLengthTypeId(binary);
+    const packetIsLiteral = packetTypeId === 4
+    if (packetIsLiteral) {
+        const toParse = binary.slice(6);
+        return parseLiteralPacket(toParse);
+    }
+    const nextPacketLength = lengthTypeId === 1 ? 11 : 15;
+    return parseOperatorPacket(maxLengthInBits);
+
+    function parseLiteralPacket(toParse) {
+        let resultToParse = "";
+        let isLast = toParse === 0;
+        let numberOfIterations = 0;
+        while (!isLast) {
+            const valueToParse = toParse.slice(1, 5);
+            isLast = toParse[0] === "0";
+            resultToParse += valueToParse;
+            toParse = toParse.slice(5);
+            numberOfIterations++;
+        }
+        const result = parseInt(resultToParse, 2);
+        const packet = {
+            packetVersion,
+            packetTypeId,
+            lengthTypeId,
+            value: result,
+            length: numberOfIterations * 5 + 6
+        };
+        return packet;
+    }
+
+    function parseOperatorPacket(maxLengthInBits) {
+        const nextPacket = binary.slice(7, 7 + nextPacketLength);
+        if (lengthTypeId === 1) return parseWithPacketCount(maxLengthInBits);
+        return parseWithPacketLength(maxLengthInBits);
+
+
+        function parseWithPacketLength(maxLengthInBits) {
+            let packetsLength = parseInt(nextPacket.padStart(16, "0"), 2);
+            let binaryToParse = binary.slice(7 + nextPacketLength);
+            const subPackets = [];
+            while (packetsLength > 0 && binary) {
+                const maxLength = maxLengthInBits ? Math.min(packetsLength, maxLengthInBits) : packetsLength;
+                const packet = parsePacket(binaryToParse, maxLength)
+                binaryToParse = binaryToParse.slice(packet.length)
+                packetsLength -= packet.length;
+                subPackets.push(packet);
+                if (maxLengthInBits) {
+                    maxLengthInBits -= packet.length;
+                    if (maxLengthInBits <= 0) break;
+                }
+            }
+            const subPacketsLength = subPackets.reduce((x, y) => x + y.length, 0)
+            const packet = {
+                packetVersion,
+                packetTypeId,
+                lengthTypeId,
+                value: subPackets,
+                length: subPacketsLength + 7 + nextPacketLength
+            };
+            return packet;
+        }
+
+        function parseWithPacketCount(maxLengthInBits) {
+            const numberOfPackets = parseInt(nextPacket, 2);
+            let binaryToParse = binary.slice(7 + nextPacketLength);
+            const subPackets = [];
+            for (let i = 0; i < numberOfPackets; i++) {
+                const packet = parsePacket(binaryToParse, maxLengthInBits)
+                binaryToParse = binaryToParse.slice(packet.length)
+                subPackets.push(packet);
+                if (maxLengthInBits) {
+                    maxLengthInBits -= packet.length;
+                    if (maxLengthInBits <= 0) break;
+                }
+            }
+            const subPacketsLength = subPackets.reduce((x, y) => x + y.length, 0)
+            const packet = {
+                packetVersion,
+                packetTypeId,
+                lengthTypeId,
+                value: subPackets,
+                length: subPacketsLength + 7 + nextPacketLength
+            };
+            return packet;
+        }
+    }
+}
+
+
+function getPacketVersion(binary) {
+    const packetVersion = binary.slice(0, 3).padStart(4, "0");
+    return parseInt(packetVersion, 2);
+}
+
+function getPacketTypeId(binary) {
+    const packetVersion = binary.slice(3, 6).padStart(4, "0");
+    return parseInt(packetVersion, 2);
+}
+
+function getLengthTypeId(binary) {
+    return +binary.slice(6, 7);
+}
+
+function hex2bin(hex) {
+    let result = "";
+    hex.split("").forEach(i => result += parseInt(i, 16).toString(2).padStart(4, "0"))
+    return result
+}
